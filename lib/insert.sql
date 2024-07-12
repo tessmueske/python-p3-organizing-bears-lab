ee7bdda16765659cb13db30757f98ba7d594064e
Tess <tess.mueske@gmail.com>
2024-07-12 12:26:13 +0200

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'happy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'F', 'black', 'social', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'black', 'social', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'F', 'polar', 'happy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'M', 'brown', 'happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'black', 'depressed', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'F', 'brown', 'happy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 7, 'M', 'white', 'quiet', 1);
